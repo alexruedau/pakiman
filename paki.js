45ff03941cdbfed04021d7bb2435700c7295bf51
Alex Rueda U <alexruedau@gmail.com>
2019-01-21 22:59:15 +0100

    We create the object instance in the push parameter

--- paki.js
+++ paki.js
@@ -3,11 +3,10 @@ var pakImages = [];
   pakImages["Pokacho"] = "pollo.png";
   pakImages["Tocinauro"] = "cerdo.png";
 
-var cauchin = new Pakiman("Cauchin", 100, 30);
-var pokacho = new Pakiman("Pokacho", 80, 50);
-var tocinauro = new Pakiman("Tocinauro", 120, 40);
-//console.log(cauchin, pokacho, tocinauro);
 
-pokacho.showIt();
-cauchin.showIt();
-tocinauro.showIt();
+var colection = [];
+colection.push(new Pakiman("Cauchin", 100, 30));
+colection.push(new Pakiman("Pokacho", 80, 50));
+colection.push(new Pakiman("Tocinauro", 120, 40));
+
+console.log(colection);
